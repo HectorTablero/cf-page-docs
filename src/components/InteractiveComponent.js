b9191061a1a3d3da6cff915b3a896d81be9f5514
= <=>
2025-01-12 19:13:22 +0100

    Bug fix

--- src/components/InteractiveComponent.js
+++ src/components/InteractiveComponent.js
@@ -5,10 +5,12 @@ export default function InteractiveComponent({ children }) {
     return (
         <BrowserOnly>
             {() => {
-                <div style={{ backgroundColor: "var(--interactive-component-background-color)", padding: "var(--interactive-component-padding)", borderRadius: "var(--ifm-global-radius)" }}>
-                    <h3>Interactive Demo</h3>
-                    {children}
-                </div>;
+                return (
+                    <div style={{ backgroundColor: "var(--interactive-component-background-color)", padding: "var(--interactive-component-padding)", borderRadius: "var(--ifm-global-radius)" }}>
+                        <h3>Interactive Demo</h3>
+                        {children}
+                    </div>
+                );
             }}
         </BrowserOnly>
     );
